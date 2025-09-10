d7be9d4f4a7c663b5c7fbbd72c394f26f0d27037
gorichire <168408037+gorichire@users.noreply.github.com>
2025-09-10 12:44:40 +0900

    옵저버 패턴

--- 옵저버패턴/옵저버.cpp
+++ 옵저버패턴/옵저버.cpp
@@ -1,24 +1,64 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <algorithm>
+
 using namespace std;
 
 class Unit {
 public:
-	virtual ~Unit() {};
-	virtual void Move(int x, int y) = 0;
-	virtual void SelectUnit() = 0;
-	virtual void DeselectUnit() = 0;
-};
-
-class Commander {
-
+    virtual ~Unit() {
+        cout << "¼Ò¸êÀÚ È£Ãâ" << endl;
+    };
+    virtual void Move(int x, int y) = 0;
 };
 
 class Marin : public Unit {
-
+public:
+    void Move(int x, int y) override {
+        cout <<x <<"," << y << "Attack" << endl;
+    }
 };
 
 class Medic : public Unit {
-
+    void Move(int x, int y) override {
+        cout <<x << "," << y << "Move" << endl;
+    }
 };
+
+class Commander {
+private:
+    vector<Unit*> selected;
+
+public:
+    void SelectUnit(Unit* u) {
+        if (u == NULL) return;
+        selected.emplace_back(u);
+    }
+
+    void DeselectUnit(Unit* u) {
+        if (u == NULL) return;
+        selected.erase(remove(selected.begin(), selected.end(), u ), selected.end());
+
+    }
+
+    void Move(int x, int y) {
+        for (Unit* u : selected) {
+            if (u) u->Move(x, y);
+        }
+    }
+};
+
+int main() {
+    Commander cmd;
+    Marin marin; Medic medic;
+
+    cmd.SelectUnit(&marin);
+    cmd.SelectUnit(&medic);
+
+    cmd.Move(10, 20);
+
+    cmd.DeselectUnit(&marin);
+    cmd.Move(0, 0);
+
+    return 0;
+}
